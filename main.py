d45490702f94498779945bd60383980b40587055
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-09-10 03:49:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ dff = pd.read_csv('fplstats2.csv')
 def path_to_image_html(path):
     return '<img src="' + path + '" width="60" >'
 
+def photo_html(path):
+    return '<img src="' + path + '" width="250" >'
+
 @st.cache
 def convert_df(input_df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -27,7 +30,7 @@ def convert_df(input_df):
 st.image(Image.open('header.png'))
 st.title("Fantasy Premier League - Artificial Intelligence")
 st.sidebar.image(Image.open('Fantasy PL-ai-logos_transparent.png'))
-menu = ["Team_Selection","Player_Analysis"]
+menu = ["Team_Selection","Player_Stats","Compare_Players"]
 choice = st.sidebar.selectbox("Select Menu", menu)
 
 if choice == "Team_Selection":
@@ -162,22 +165,94 @@ if choice == "Team_Selection":
             if captmain[i].value()==1:
                 st.write(f'Captain: {names[i]}')
 
-elif choice=="Player_Analysis":
-    k1,k2 = st.columns((3,2))
+elif choice == "Player_Stats":
+    k1, k2 = st.columns((1, 1))
     with k1:
-        # AgGrid(df,fit_columns_on_grid_load=True)
-        df['Points']=df['Total_Points']
-        df1 = df.drop(['Photo','Total_Points'], axis=1)
-        st.dataframe(df1)
-        # html = convert_df(df)
-        # st.markdown(
-        #     html,
-        #     unsafe_allow_html=True
-        # )
-        # st._arrow_dataframe(df.style.highlight_max(axis=0))
-    with k2:
         dfpoint = df.nlargest(5, 'Total_Points')
-        dfpoint=dfpoint.sort_values('Total_Points', ascending = True)
-        fig = px.bar(dfpoint,y='Name',x='Total_Points',orientation='h')
-        st.plotly_chart (fig)
+        dfpoint = dfpoint.sort_values('Total_Points', ascending=True)
+        fig1a = px.bar(dfpoint, y='Name', x='Total_Points', orientation='h',title='Top Points')
+        st.plotly_chart(fig1a)
+        # dfgoal = df.nlargest(5, 'Form')
+        # dfgoal = dfgoal.sort_values('Goals', ascending=True)
+        # fig1b = px.bar(dfgoal, y='Name', x='Goals', orientation='h', title='Top Goals')
+        # st.plotly_chart(fig1b)
+    with k2:
+        dfform = df.nlargest(5, 'Form')
+        dfform = dfform.sort_values('Form', ascending=True)
+        fig2a = px.bar(dfform, y='Name', x='Form', orientation='h', title='Top Forms')
+        st.plotly_chart(fig2a)
+        # dfassist = df.nlargest(5, 'Assists')
+        # dfassist = dfassist.sort_values('Assists', ascending=True)
+        # fig2b = px.bar(dfassist, y='Name', x='Assists', orientation='h', title='Top Assists')
+        # st.plotly_chart(fig2b)
+    df['Points'] = df['Total_Points']
+    df1 = df.drop(['Photo','Total_Points'], axis=1)
+    # AgGrid(df1,fit_columns_on_grid_load=True)
+    st.dataframe(df1)
+elif choice=="Compare_Players":
+    from plotly import graph_objects as go
+    p1,p2 = st.columns((1,1))
+    with p1:
+        pl = st.selectbox('Select Player 1',df['Name'])
+        if pl == []:
+            st.write('Please Select A Player')
+        else:
+           df2 = df[df['Name']==pl]
+           dff2 = dff[dff['Name']==pl]
+           st.markdown(
+               photo_html(df2['Photo'].tolist()[0]),
+               unsafe_allow_html=True
+           )
+           # st.write(dff2)
+           fig = go.Figure()
+           categories = ['goals','assists','clean_sheets','inv_fdr','ppg']
+           fig.add_trace(go.Scatterpolar(
+               r=[df2['Goals'].tolist()[0],df2['Assists'].tolist()[0],df2['Clean_Sheets'].tolist()[0],dff2['Inv_FDR'].tolist()[0],dff2['Point_per_game'].tolist()[0]],
+               theta=categories,
+               fill='toself',
+               name='Product A'
+           ))
+           fig.update_layout(
+               polar=dict(
+                   radialaxis=dict(
+                       visible=True,
+                       range=[0, dff['Point_per_game'].max()]
+                   )),
+               showlegend=False
+           )
+           st.plotly_chart(fig)
+           df_merged = df2[['Total_Points','Goals','Assists','Clean_Sheets']].melt().append(dff2[['Inv_FDR','Point_per_game','ICT_index']].melt())
+           st.table(df_merged[['variable','value']])
+    with p2:
+        pl2 = st.selectbox('Select Player 2',df['Name'])
+        # if pl2!=[]:
+        if pl2==[]:
+           st.write('Please Select A Player')
+        else:
+           df2a = df[df['Name']==pl2]
+           dff2a = dff[dff['Name']==pl2]
+           st.markdown(
+               photo_html(df2a['Photo'].tolist()[0]),
+               unsafe_allow_html=True
+           )
+           # st.write(dff2)
+           fig2 = go.Figure()
+           categories = ['goals','assists','clean_sheets','inv_fdr','ppg']
+           fig2.add_trace(go.Scatterpolar(
+               r=[df2a['Goals'].tolist()[0],df2a['Assists'].tolist()[0],df2a['Clean_Sheets'].tolist()[0],dff2a['Inv_FDR'].tolist()[0],dff2a['Point_per_game'].tolist()[0]],
+               theta=categories,
+               fill='toself',
+               name='Product A'
+           ))
+           fig2.update_layout(
+               polar=dict(
+                   radialaxis=dict(
+                       visible=True,
+                       range=[0, dff['Point_per_game'].max()]
+                   )),
+               showlegend=False
+           )
+           st.plotly_chart(fig2)
+           df_merged2 = df2a[['Total_Points','Goals','Assists','Clean_Sheets']].melt().append(dff2a[['Inv_FDR','Point_per_game','ICT_index']].melt())
+           st.table(df_merged2[['variable','value']])
 
